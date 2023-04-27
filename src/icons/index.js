c0c75f703b3b0ca8699fb5c16a2f865578021618
Yerkebulan <128957554+yerkebulan0@users.noreply.github.com>
2023-04-27 12:39:34 +0600

    code clean

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,12 @@
-import {ReactComponent as HomeIcon} from './home.svg';
+import {ReactComponent as HomeIcon} from './Home.svg';
+import {ReactComponent as IconFig} from './IconFig.svg';
+import {ReactComponent as Trird} from './Trird.svg';
+import {ReactComponent as Four} from './Four.svg';
+import {ReactComponent as Fif} from './Fif.svg';
+import {ReactComponent as Si} from './Si.svg';
+import {ReactComponent as Comment} from './Commet.svg';
+import {ReactComponent as Repost} from './Repost.svg';
+import {ReactComponent as Like} from './Like.svg';
 
-export {HomeIcon  };
+
+export {HomeIcon,IconFig,Trird,Four,Fif,Si,Comment  ,Repost,Like};
